7c4766b39f808feb511dac7a445c9e41a4aedf16
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-06-26 13:51:03 +0300

    first commit

---
+++ dist/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { createServer } from 'node:http';
+// import { fileURLToPath } from 'node:url';
+// import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+const app = express();
+const server = createServer(app);
+const socket = new Server(server);
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+});
+socket.on('connection', (connection) => {
+    console.log(connection);
+});
+const PORT = process.env.PORT || 3009;
+server.listen(PORT, () => {
+    console.log('server running at http://localhost:3009');
+});
+//# sourceMappingURL=app.js.map
